c627f9830895c347b2441677445fb40ed79c7199
A B <abbowman21@hotmail.com>
2020-06-02 15:53:20 -0700

    Initial Commit

---
+++ DeDupeFile.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Skribblio_List
+{
+    static class DeDupeFile
+    {
+        public static int RemoveDuplicatesFromAndWriteOutToFile(string fileName)
+        {
+            FileInfo file = new FileInfo(fileName);
+
+            if (!file.Exists)
+            {
+                throw new FileNotFoundException("File not found", fileName);
+            }
+
+            int duplicateCount;
+            HashSet<string> entries = GetEntriesFromFile(file, out duplicateCount);
+
+            WriteOutDeDupedEntries(file, entries);
+
+            return duplicateCount;
+        }
+
+        private static HashSet<string> GetEntriesFromFile(FileInfo file, out int dupeCount)
+        {
+            dupeCount = 0;
+            string fullText = File.ReadAllText(file.FullName).Trim();
+
+            string[] allWords = fullText.Split(new char[] { ',', '\n' });
+
+            HashSet<string> entries = new HashSet<string>(allWords, StringComparer.OrdinalIgnoreCase);
+
+            dupeCount = allWords.Length - entries.Count;
+            return entries;
+        }
+
+        private static void WriteOutDeDupedEntries(FileInfo file, HashSet<string> entries)
+        {
+            string csvOutput = String.Join(",", entries);
+            File.WriteAllText(file.FullName, csvOutput, Encoding.ASCII);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows;
+
+namespace Skribblio_List
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DeDupe_button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string file = this.wordList_box.Text.Trim(new char[] { ' ', '"' });
+
+                int duplicates = DeDupeFile.RemoveDuplicatesFromAndWriteOutToFile(file);
+
+                PopSuccessMessageBox(duplicates);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+        }
+
+        private static void PopSuccessMessageBox(int duplicates)
+        {
+            if (duplicates == 0)
+            {
+                MessageBox.Show("0 duplicates found.", "Success",
+                MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show(String.Format("{0} duplicates removed.", duplicates),
+                "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+    }
+}
